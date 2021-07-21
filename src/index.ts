e687ff9ea0adf5e533ae8fc1f1cc14da81541beb
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2021-07-21 13:09:05 +0300

    Add asciichart

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,55 @@
-import InvestSDK from '@tinkoff/invest-openapi-js-sdk'
-import { startOfToday, endOfToday } from 'date-fns'
+import InvestSDK, { CandleStreaming } from '@tinkoff/invest-openapi-js-sdk'
+import { sub, parseISO, format } from 'date-fns'
+import { plot as chart, red, green } from 'asciichart'
 
 import config from './config'
 
 const api = new InvestSDK(config.api)
 await api.sandboxClear()
 
-const { figi } = await api.searchOne({ ticker: config.ticker })
+const { figi, currency } = await api.searchOne({ ticker: config.ticker })
 
-const { candles } = await api.candlesGet({
+// get init candles
+const { candles: initCandles } = await api.candlesGet({
   figi,
-  from: startOfToday().toISOString(),
-  to: endOfToday().toISOString(),
-  interval: 'hour',
+  from: sub(new Date(), { minutes: 10 }).toISOString(),
+  to: new Date().toISOString(),
+  interval: '1min',
 })
 
-console.log(candles[candles.length - 1])
+const candles: CandleStreaming[] = initCandles
+let max = Math.max(...candles.map(({ o }) => o))
 
-// api.candle({ figi }, (candle) => {
-//   console.log(candle)
-// })
+api.candle({ figi, interval: '1min' }, (candle) => {
+  if (candles.findIndex(({ time }) => time === candle.time) === -1) {
+    candles.push(candle)
 
-// api.orderbook({ figi, depth: 10 }, (data) => {
-//   console.log(data)
-// })
+    if (candles.length) {
+      const data = candles.map((candle) => candle.o)
+
+      console.log(
+        chart([data.map(() => max), data], {
+          padding: '',
+          offset: 2,
+          height: 6,
+          colors: [red, undefined, green],
+        })
+      )
+
+      console.log(
+        '\n',
+        format(parseISO(candle.time), 'k:mm'),
+        `Price: ${candle.o} ${currency}`,
+        '\n'
+      )
+
+      if (candle.o > max) {
+        console.log('New maximum')
+
+        max = candle.o
+      }
+    }
+  }
+
+  return true
+})
