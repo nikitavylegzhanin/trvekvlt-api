866066dbe7b36e03e4bc2256811eed651e040123
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2022-01-08 09:48:13 +0300

    Test env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { connect } from './db'
 import { initApp, subscribePrice, startReduxDevTool } from './app'
 
+console.log('Test env TICKER', process.env.TICKER)
+
 connect()
   .then(initApp)
   .then(subscribePrice)
