ea40f9d563a03637fb72dc25a21569172b3eb7a3
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2022-05-23 18:43:30 +0300

    Do not save state log

--- src/store/logMiddleware.ts
+++ src/store/logMiddleware.ts
@@ -1,7 +1,6 @@
 import { Middleware } from '@reduxjs/toolkit'
-import { getConnection } from 'typeorm'
 
-import { Log, LogType } from '../db'
+import { LogType } from '../db'
 import { sendMessage } from '../telegram'
 
 const logMiddleware: Middleware = (_store) => (next) => (action) => {
@@ -10,14 +9,6 @@ const logMiddleware: Middleware = (_store) => (next) => (action) => {
     const message = JSON.stringify(action)
 
     sendMessage(type, message)
-
-    const { manager } = getConnection()
-    manager.save(
-      manager.create(Log, {
-        type,
-        message,
-      })
-    )
   }
 
   return next(action)
