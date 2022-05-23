538ffff45ef808d875af57e2453c68ee285c1c1f
Nikita Vylegzhanin <vylegzhanin.nikita@gmail.com>
2022-05-23 19:14:00 +0300

    Format state messages

--- src/store/logMiddleware.ts
+++ src/store/logMiddleware.ts
@@ -1,14 +1,31 @@
-import { Middleware } from '@reduxjs/toolkit'
+import { Middleware, PayloadAction } from '@reduxjs/toolkit'
+import { pick } from 'ramda'
 
+import { BotActionType, StoredBot } from './bots'
 import { LogType } from '../db'
 import { sendMessage } from '../telegram'
 
+const formatMessage = (action: PayloadAction<any>) => {
+  switch (action.type) {
+    case BotActionType.INIT: {
+      const { payload } = action as PayloadAction<StoredBot[]>
+
+      return payload.reduce(
+        (message, bot) =>
+          message.concat(
+            JSON.stringify(pick(['name', 'ticker', 'positions'], bot))
+          ),
+        ''
+      )
+    }
+    default:
+      return JSON.stringify(action.payload)
+  }
+}
+
 const logMiddleware: Middleware = (_store) => (next) => (action) => {
   if (process.env.NODE_ENV !== 'test') {
-    const type = LogType.STATE
-    const message = JSON.stringify(action)
-
-    sendMessage(type, message)
+    sendMessage(LogType.STATE, formatMessage(action))
   }
 
   return next(action)
